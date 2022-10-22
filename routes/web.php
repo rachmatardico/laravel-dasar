e15771ace5d011e33892f3aeaa1f3adb31a2c34c
Rachmat Ardico Perdana <rahmatardiko99@gmail.com>
2022-10-22 15:01:34 +0700

    Input Type

--- routes/web.php
+++ routes/web.php
@@ -86,3 +86,4 @@ Route::post('/input/hello', [InputController::class, 'hello']);
 Route::post('/input/hello/first', [InputController::class, 'helloFirstName']);
 Route::post('/input/hello/input', [InputController::class, 'helloInput']);
 Route::post('/input/hello/array', [InputController::class, 'helloArray']);
+Route::post('/input/type', [InputController::class, 'inputType']);
--- tests/Feature/InputControllerTest.php
+++ tests/Feature/InputControllerTest.php
@@ -58,4 +58,14 @@ class InputControllerTest extends TestCase
         ])->assertSeeText("Apple Mac Book Pro")
             ->assertSeeText("Samsung Galaxy S10");
     }
+    
+    public function testInputType()
+    {
+        $this->post('/input/type', [
+            "name"          => "Budi",
+            "married"       => true,
+            "birth_date"    => '1990-10-10'
+        ])->assertSeeText("Budi")->assertSeeText("true")
+        ->assertSeeText("1990-10-10");
+    }
 }
