ac59049b3b0d1e0269a8f6c4d0e39e10ae49f5cf
Rachmat Ardico Perdana <rahmatardiko99@gmail.com>
2022-10-22 15:00:32 +0700

    Request Input

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use App\Http\Controllers\HelloController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HelloController;
+use App\Http\Controllers\InputController;
 
 /*
 |--------------------------------------------------------------------------
@@ -74,6 +75,14 @@ Route::get('/produk-redirect/{id}', function($id){
     return redirect()->route('product.detail', ['id' => $id]);
 });
 
+// Request
 Route::get('/controller/hello/request', [HelloController::class, 'request']);
-
 Route::get('/controller/hello/{name}', [HelloController::class, 'hello']);
+
+// Request Input
+Route::get('/input/hello', [InputController::class, 'hello']);
+Route::post('/input/hello', [InputController::class, 'hello']);
+
+Route::post('/input/hello/first', [InputController::class, 'helloFirstName']);
+Route::post('/input/hello/input', [InputController::class, 'helloInput']);
+Route::post('/input/hello/array', [InputController::class, 'helloArray']);
---
+++ tests/Feature/InputControllerTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class InputControllerTest extends TestCase
+{
+    public function testInput()
+    {
+        $this->get("/input/hello?name=Rachmat")
+            ->assertSeeText("Hello Rachmat");
+        
+        $this->post("/input/hello", [
+            "name"  => "Rachmat"
+        ])->assertSeeText("Hello Rachmat");
+    }
+
+    public function testInputNested()
+    {
+        $this->post("/input/hello", [
+            "name"  => [
+                "first" => "Rachmat",
+                "last"  => "Ardico"
+            ]
+        ])->assertSeeText("Hello Rachmat");
+    }
+
+    public function testInputAll()
+    {
+        $this->post("/input/hello/input", [
+            "name"  => [
+                "first" => "Rachmat",
+                "last"  => "Ardico"
+            ]
+        ])->assertSeeText("name")->assertSeeText("first")
+            ->assertSeeText("last")
+            ->assertSeeText("Rachmat")
+            ->assertSeeText("Ardico");
+    }
+
+    public function testInputArray()
+    {
+        $this->post("/input/hello/array", [
+            "products"  => [
+                [
+                    "name"  =>  "Apple Mac Book Pro",
+                    "price" =>  30000000
+                ],
+                [
+                    "name"  =>  "Samsung Galaxy S10",
+                    "price" =>  15000000
+                ]
+            ]
+        ])->assertSeeText("Apple Mac Book Pro")
+            ->assertSeeText("Samsung Galaxy S10");
+    }
+}
