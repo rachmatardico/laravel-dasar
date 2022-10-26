937c0515f194304f00fe4dca4b5705f347d69b8a
Rachmat Ardico Perdana <rahmatardiko99@gmail.com>
2022-10-26 12:15:33 +0700

    Response

--- routes/web.php
+++ routes/web.php
@@ -97,3 +97,11 @@ Route::post('/input/filter/merge', [InputController::class, 'filterMerge']);
 
 // File Upload
 Route::post('/file/upload', [\App\Http\Controllers\FileController::class, 'upload']);
+
+// Response
+Route::get('/response/hello', [\App\Http\Controllers\ResponseController::class, 'response']);
+Route::get('/response/header', [\App\Http\Controllers\ResponseController::class, 'header']);
+Route::get('/response/type/view', [\App\Http\Controllers\ResponseController::class, 'responseView']);
+Route::get('/response/type/json', [\App\Http\Controllers\ResponseController::class, 'responseJson']);
+Route::get('/response/type/file', [\App\Http\Controllers\ResponseController::class, 'responseFile']);
+Route::get('/response/type/download', [\App\Http\Controllers\ResponseController::class, 'responseDownload']);
